53caa4ac3a89475feeccc55bcf001d404f2ca6bb
chencailong <chencailong@chargefrog.com>
2020-06-19 09:29:55 +0800

    使用orm查询配置在config下的数据来进行匹配解密sessionkey

--- app/api/uniapp/articleAction.go
+++ app/api/uniapp/articleAction.go
@@ -1,8 +1,11 @@
 package uniapp
 
 import (
+	"github.com/gogf/gf/encoding/gparser"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/os/glog"
+	"github.com/xlstudio/wxbizdatacrypt"
 	"gmanager/library/base"
 )
 
@@ -15,44 +18,49 @@ type Response struct {
 	Openid     string `form:"openid",json:"openid"`
 }
 
+type ConfigList struct {
+	Id     int    `orm:"id"`
+	Name   string `orm:"name"`
+	Appid  string `orm:"appid"`
+	Secret string `orm:"secret"`
+}
+
 func (action *ArticleAction) Index(r *ghttp.Request) {
 	form := base.NewForm(r.GetMap())
-	db := g.DB()
-	list, _ := db.GetOne("select * from waluduo_mp_wechat_config where id = ?", g.Slice{form.Config_id})
-	base.Succ(r, g.Map{
-		"post": list,
-	})
-	//if s, err := ghttp.Get("https://api.weixin.qq.com/sns/jscode2session?appid=" + configList.Appid + "&secret=" + configList.secret + "&js_code=" + form.Code + "&grant_type=authorization_code"); err != nil {
-	//	glog.Error(err)
-	//} else {
-	//	//这个方法是将返回数据对应到struct
-	//	//if j, errr := gjson.DecodeToJson(s.ReadAllString()); errr != nil {
-	//	//	panic(errr)
-	//	//} else {
-	//	//	response := new(Response)
-	//	//	if err := j.ToStruct(response); err != nil {
-	//	//		panic(err)
-	//	//	}
-	//	//	base.Succ(r, g.Map{
-	//	//		"post": form,
-	//	//		"result": response,
-	//	//	})
-	//	//}
-	//
-	//	if p, e := gparser.LoadContent([]byte(s.ReadAllString())); e != nil {
-	//		glog.Error(e)
-	//	} else {
-	//		pc := wxbizdatacrypt.WxBizDataCrypt{AppID: form.Appid, SessionKey: p.GetString("session_key")}
-	//		result, errr := pc.Decrypt(form.EncryptedData, form.Iv, false) //第三个参数解释： 需要返回 JSON 数据类型时 使用 true, 需要返回 map 数据类型时 使用 false
-	//		if errr != nil {
-	//			glog.Error(errr)
-	//		} else {
-	//			base.Succ(r, g.Map{
-	//				"post":   form,
-	//				"result": result,
-	//			})
-	//		}
-	//	}
-	//	s.Close()
-	//}
+	var configList *ConfigList
+	//ConfigList, _ := db.GetOne("select * from waluduo_mp_wechat_config where id = ?", g.Slice{form.Config_id})
+	_ = g.DB().Table("waluduo_"+form.Platform+"_config").Where("id", form.Config_id).Scan(&configList)
+	if s, err := ghttp.Get("https://api.weixin.qq.com/sns/jscode2session?appid=" + configList.Appid + "&secret=" + configList.Secret + "&js_code=" + form.Code + "&grant_type=authorization_code"); err != nil {
+		glog.Error(err)
+	} else {
+		//这个方法是将返回数据对应到struct
+		//if j, errr := gjson.DecodeToJson(s.ReadAllString()); errr != nil {
+		//	panic(errr)
+		//} else {
+		//	response := new(Response)
+		//	if err := j.ToStruct(response); err != nil {
+		//		panic(err)
+		//	}
+		//	base.Succ(r, g.Map{
+		//		"post": form,
+		//		"result": response,
+		//	})
+		//}
+
+		if p, e := gparser.LoadContent([]byte(s.ReadAllString())); e != nil {
+			glog.Error(e)
+		} else {
+			pc := wxbizdatacrypt.WxBizDataCrypt{AppID: configList.Appid, SessionKey: p.GetString("session_key")}
+			result, errr := pc.Decrypt(form.EncryptedData, form.Iv, false) //第三个参数解释： 需要返回 JSON 数据类型时 使用 true, 需要返回 map 数据类型时 使用 false
+			if errr != nil {
+				glog.Error(errr)
+			} else {
+				base.Succ(r, g.Map{
+					"post":   form,
+					"result": result,
+				})
+			}
+		}
+		_ = s.Close()
+	}
 }
