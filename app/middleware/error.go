e4a7d39a5f9fd4b156887d602b0a76293bfe0950
chencailong <chencailong@chargefrog.com>
2020-11-03 16:25:48 +0800

    主程序注册流程

--- app/middleware/error.go
+++ app/middleware/error.go
@@ -6,14 +6,13 @@ import (
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
-
+//定义错误处理方法
 func Error(r *ghttp.Request) {
-	r.Middleware.Next()
-	if err := r.GetError(); err != nil {
-		// 记录到自定义错误日志文件
-		g.Log("exception").Error(err)
+	r.Middleware.Next()//后置中间件
+	if err := r.GetError(); err != nil {//判断是否获取到错误
+		g.Log("exception").Error(err)// 记录到自定义错误日志文件
 		//返回固定的友好信息
-		r.Response.ClearBuffer()
+		r.Response.ClearBuffer()//将原本缓冲区的输出内容清空
 		global.FailWithMessage(r, "服务器居然开小差了，请稍后再试吧！")
 	}
 }
--- boot/server.go
+++ boot/server.go
@@ -1,7 +1,6 @@
 package boot
 
 import (
-	"fmt"
 	"server/app/middleware"
 	"server/router"
 	"time"
@@ -10,21 +9,21 @@ import (
 )
 
 func InitializeRunServer() {
-	s := g.Server()
-	s.Use(middleware.Error)
-	s.SetReadTimeout(10 * time.Second)
-	s.SetWriteTimeout(10 * time.Second)
-	s.SetMaxHeaderBytes(1 << 20)
-	s.SetIndexFolder(true)
+	s := g.Server()//初始化服务
+	s.Use(middleware.Error)//全局路由规则注册这个中间件
+	s.SetReadTimeout(10 * time.Second)//读取延时
+	s.SetWriteTimeout(10 * time.Second)//写入延时
+	s.SetMaxHeaderBytes(1 << 20)//内存地址每位向左移一位，1*2的20次方，也就是1Mb
+	s.SetIndexFolder(true)//当访问静态文件目录时，是否展示目录下的文件列表。默认关闭，那么请求将返回403
 	//if g.Cfg("system").GetString("system.OssType") == "local" {
 	//	s.AddStaticPath("/"+g.Cfg("oss").GetString("local.LocalPath"), g.Cfg("oss").GetString("local.LocalPath"))
 	//}
-	s.AddStaticPath("/form-generator", "public/page")
-	router.InitializeRouters()
-	fmt.Printf(`
-	欢迎使用 Gf-Vue-Admin
-	当前版本:V1.1.2
-	默认前端文件运行地址:http://127.0.0.1:8080
-	`)
+	s.AddStaticPath("/form-generator", "public/page")//为添加URI与目录路径的映射关系，可以自定义静态文件目录的访问URI规则
+	router.InitializeRouters()//初始化总路由
+	//fmt.Printf(`
+	//欢迎使用 Gf-Vue-Admin
+	//当前版本:V1.1.2
+	//默认前端文件运行地址:http://127.0.0.1:8080
+	//`)
 	s.Run()
 }
